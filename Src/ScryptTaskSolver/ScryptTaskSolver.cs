b283d469cf4f108af01c0fac511b5187e4d6ffe9
z0rg1nc <z0rg1nc@users.noreply.github.com>
2015-12-04 07:05:35 -0500

    Orig files

---
+++ Src/ScryptTaskSolver/ScryptTaskSolver.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using BtmI2p.ComputableTaskInterfaces.Client;
+using BtmI2p.CryptSharp.Utility;
+using BtmI2p.MiscUtils;
+using NLog;
+
+namespace BtmI2p.TaskSolvers.Scrypt
+{
+    public static class ScryptTaskSolver
+    {
+        private static void LoadResourceToCurrentFolder(
+            string fileName
+        )
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            // EndsWith - because of ILRepack
+            if (!assembly.GetManifestResourceNames().Any(x => x.EndsWith(fileName)))
+                throw new Exception("Resource not found");
+            var resourceName = assembly.GetManifestResourceNames().First(
+                x => x.EndsWith(fileName)
+            );
+            using (var resource = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (resource == null)
+                    throw new NullReferenceException("resource");
+                using (var fs = new FileStream(Path.Combine(".", fileName), FileMode.Create))
+                {
+                    const int bufSize = 4096;
+                    var buffer = new byte[bufSize];
+                    int readBytes;
+                    while ((readBytes = resource.Read(buffer, 0, bufSize)) > 0)
+                    {
+                        fs.Write(buffer, 0, readBytes);
+                    }
+                }
+            }
+        }
+
+        static ScryptTaskSolver()
+        {
+            try
+            {
+                var soFileName = IntPtr.Size == 8 
+                    ? "libscrypt_x64.so" 
+                    : "libscrypt_x86.so";
+                LoadResourceToCurrentFolder(
+                    soFileName
+                );
+                var dllFileName = IntPtr.Size == 8
+                    ? "ScryptNativeWinDllx64.dll"
+                    : "ScryptNativeWinDllx86.dll";
+                LoadResourceToCurrentFolder(
+                    dllFileName
+                );
+            }
+            catch (Exception exc)
+            {
+                _log.Error(
+                    "ScryptTaskSolver ctor unexpected error '{0}'",
+                    exc.ToString()
+                );
+            }
+        }
+
+        private static bool CheckScryptOutput(
+            byte[] res,
+            byte[] mask,
+            byte[] rightSolutionBytes
+            )
+        {
+            if(res == null || mask == null || rightSolutionBytes == null)
+                throw new ArgumentNullException();
+            if(res.Length == 0)
+                throw new ArgumentOutOfRangeException();
+            int n = res.Length;
+            if(n != mask.Length || n != rightSolutionBytes.Length)
+                throw new ArgumentException();
+            for (int i = 0; i < n; i++)
+            {
+                if ((res[i] & mask[i]) != (rightSolutionBytes[i] & mask[i]))
+                    return false;
+            }
+            return true;
+        }
+
+        public enum EUseNativeScrypt
+        {
+            None,
+            WinDll,
+            LinuxSo
+        }
+
+        [DllImport(
+            "libscrypt_x86.so",
+            EntryPoint = "libscrypt_scrypt",
+            CallingConvention = CallingConvention.Cdecl
+        )]
+        private static extern int LibscryptScrypt86(
+            byte[] passwd,
+            uint passwdlen,
+            byte[] salt,
+            uint saltlen,
+            ulong n,
+            uint r,
+            uint p,
+            byte[] buf,
+            uint buflen
+            );
+
+        [DllImport(
+            "libscrypt_x64.so",
+            EntryPoint = "libscrypt_scrypt",
+            CallingConvention = CallingConvention.Cdecl
+        )]
+        private static extern long LibscryptScrypt64(
+            byte[] passwd,
+            ulong passwdlen,
+            byte[] salt,
+            ulong saltlen,
+            ulong n,
+            uint r,
+            uint p,
+            byte[] buf,
+            ulong buflen
+            );
+
+        private static byte[] LinuxNativeComputeScrypt(
+            byte[] passBytes,
+            ScryptTaskDescription taskDescription
+            )
+        {
+            var result = new byte[taskDescription.DkLen];
+            if (IntPtr.Size == 8)
+            {
+                if (LibscryptScrypt64(
+                    passBytes,
+                    (ulong)passBytes.Length,
+                    taskDescription.Salt,
+                    (ulong)taskDescription.Salt.Length,
+                    (ulong)taskDescription.N,
+                    (uint)taskDescription.R,
+                    (uint)taskDescription.P,
+                    result,
+                    (ulong)taskDescription.DkLen
+                    ) != 0
+                    )
+                {
+                    throw new Exception("Some LibscryptScrypt64 internal error");
+                }
+            }
+            else
+            {
+                if (LibscryptScrypt86(
+                    passBytes,
+                    (uint)passBytes.Length,
+                    taskDescription.Salt,
+                    (uint)taskDescription.Salt.Length,
+                    (ulong)taskDescription.N,
+                    (uint)taskDescription.R,
+                    (uint)taskDescription.P,
+                    result,
+                    (uint)taskDescription.DkLen
+                    ) != 0
+                    )
+                {
+                    throw new Exception("Some LibscryptScrypt86 internal error");
+                }
+            }
+            return result;
+        }
+
+        [DllImport(
+            "ScryptNativeWinDllx86.dll",
+            EntryPoint = "crypto_scrypt",
+            CallingConvention = CallingConvention.Cdecl
+        )]
+        private static extern int WindowsScrypt86(
+            byte[] passwd,
+            uint passwdlen,
+            byte[] salt,
+            uint saltlen,
+            ulong n,
+            uint r,
+            uint p,
+            byte[] buf,
+            uint buflen
+            );
+
+        [DllImport(
+            "ScryptNativeWinDllx64.dll",
+            EntryPoint = "crypto_scrypt",
+            CallingConvention = CallingConvention.Cdecl
+        )]
+        private static extern long WindowsScrypt64(
+            byte[] passwd,
+            ulong passwdlen,
+            byte[] salt,
+            ulong saltlen,
+            ulong n,
+            uint r,
+            uint p,
+            byte[] buf,
+            ulong buflen
+            );
+        private static byte[] WindowsNativeComputeScrypt(
+            byte[] passBytes,
+            ScryptTaskDescription taskDescription
+        )
+        {
+            var result = new byte[taskDescription.DkLen];
+            if (IntPtr.Size == 8)
+            {
+                if (WindowsScrypt64(
+                    passBytes,
+                    (ulong)passBytes.Length,
+                    taskDescription.Salt,
+                    (ulong)taskDescription.Salt.Length,
+                    (ulong)taskDescription.N,
+                    (uint)taskDescription.R,
+                    (uint)taskDescription.P,
+                    result,
+                    (ulong)taskDescription.DkLen
+                    ) != 0
+                    )
+                {
+                    throw new Exception("Some LibscryptScrypt64 internal error");
+                }
+            }
+            else
+            {
+                if (WindowsScrypt86(
+                    passBytes,
+                    (uint)passBytes.Length,
+                    taskDescription.Salt,
+                    (uint)taskDescription.Salt.Length,
+                    (ulong)taskDescription.N,
+                    (uint)taskDescription.R,
+                    (uint)taskDescription.P,
+                    result,
+                    (uint)taskDescription.DkLen
+                    ) != 0
+                    )
+                {
+                    throw new Exception("Some LibscryptScrypt86 internal error");
+                }
+            }
+            return result;
+        }
+
+        public static byte[] ScryptCompute(
+            byte[] passBytes, 
+            ScryptTaskDescription taskDescription, 
+            EUseNativeScrypt nativeSupport = EUseNativeScrypt.None
+        )
+        {
+            if (nativeSupport == EUseNativeScrypt.None)
+            {
+                return SCrypt.ComputeDerivedKey(
+                    passBytes,
+                    taskDescription.Salt,
+                    taskDescription.N,
+                    taskDescription.R,
+                    taskDescription.P,
+                    1,
+                    taskDescription.DkLen
+                );
+            }
+            if (nativeSupport == EUseNativeScrypt.WinDll)
+            {
+                return WindowsNativeComputeScrypt(
+                    passBytes,
+                    taskDescription
+                );
+            }
+            if (nativeSupport == EUseNativeScrypt.LinuxSo)
+            {
+                return LinuxNativeComputeScrypt(
+                    passBytes,
+                    taskDescription
+                );
+            }
+            throw new ArgumentOutOfRangeException(
+                "nativeSupport"
+            );
+        }
+
+        public class SolveScryptTaskDebugInfo
+        {
+            public int IterationCount = 0;
+        }
+        public static async Task<ComputableTaskSolution<ScryptTaskSolution>> SolveScryptTask(
+            ComputableTaskDescription<ScryptTaskDescription> taskDescription,
+            CancellationToken cancellationToken,
+            int maxThreads = 1,
+            EUseNativeScrypt nativeSupport = EUseNativeScrypt.None,
+            SolveScryptTaskDebugInfo debugInfo = null
+        )
+        {
+            var lockSem = new SemaphoreSlim(maxThreads);
+            var resultTask = new TaskCompletionSource<ComputableTaskSolution<ScryptTaskSolution>>();
+            var tasks = new Task[maxThreads];
+            for (int i = 0; i < maxThreads; i++)
+            {
+                tasks[i] = Task.Run(
+                    () =>
+                    {
+                        var passBytes = new byte
+                            [taskDescription.TaskDescription.PassSaltSize];
+                        while (
+                            !cancellationToken.IsCancellationRequested 
+                            && !resultTask.Task.IsCompleted
+                            && taskDescription.CommonInfo.ValidUntil > DateTime.UtcNow
+                        )
+                        {
+                            MiscFuncs.GetRandomBytes(passBytes);
+                            var res = ScryptCompute(
+                                passBytes,
+                                taskDescription.TaskDescription,
+                                nativeSupport
+                            );
+                            if (debugInfo != null)
+                                Interlocked.Increment(ref debugInfo.IterationCount);
+                            if (
+                                CheckScryptOutput(
+                                    res,
+                                    taskDescription.TaskDescription.SolutionMask,
+                                    taskDescription.TaskDescription.SolutionEqual
+                                )
+                            )
+                            {
+                                var result = new ComputableTaskSolution<ScryptTaskSolution>()
+                                {
+                                    CommonInfo = taskDescription.CommonInfo,
+                                    TaskSolution = new ScryptTaskSolution()
+                                    {
+                                        SolutionPass = passBytes
+                                    }
+                                };
+                                resultTask.TrySetResult(
+                                    result
+                                );
+                                return;
+                            }
+                        }
+                        resultTask.TrySetCanceled();
+                    }
+                );
+            }
+            return await resultTask.Task.ConfigureAwait(false);
+        }
+
+        private static readonly Logger _log
+            = LogManager.GetCurrentClassLogger();
+        public async static Task<bool> CheckTaskSolution(
+            ComputableTaskDescription<ScryptTaskDescription> taskDescription,
+            ComputableTaskSolution<ScryptTaskSolution> taskSolution
+        )
+        {
+            if(taskDescription == null || taskSolution == null)
+                throw new ArgumentNullException();
+            if (taskDescription.CommonInfo.TaskGuid != taskSolution.CommonInfo.TaskGuid)
+                throw new ArgumentOutOfRangeException(
+                    "taskDescription.TaskGuid != taskSolution.TaskGuid"
+                );
+            var passBytes = taskSolution.TaskSolution.SolutionPass;
+            var res = await Task.Factory.StartNew(
+                () => ScryptCompute(
+                    passBytes, 
+                    taskDescription.TaskDescription
+                ), 
+                TaskCreationOptions.LongRunning
+            ).ConfigureAwait(false);
+            var result = CheckScryptOutput(
+                res, 
+                taskDescription.TaskDescription.SolutionMask,
+                taskDescription.TaskDescription.SolutionEqual
+            );
+            return result;
+        }
+    }
+}
---
+++ Tests/TestComputableTaskSolvers/TestTaskSolvers.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using BtmI2p.MiscUtils;
+using BtmI2p.ComputableTaskInterfaces.Client;
+using BtmI2p.TaskSolvers.Scrypt;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TestClientExternalCommunication
+{
+    public class TestTaskSolvers
+    {
+        private readonly ITestOutputHelper _output;
+        public TestTaskSolvers(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public async Task Test1()
+        {
+            var taskB64 ="ew0KICAiQ29tbW9uSW5mbyI6IHsNCiAgICAiVGFza0d1aWQiOiAiNjM3MDQ4MjMtY2E3NS00MGU5LTkyMTgtMDAzYmJiZTY1NzIyIiwNCiAgICAiVmFsaWRVbnRpbCI6ICIyMDE1LTA3LTEwVDExOjQ3OjE2IiwNCiAgICAiVGFza1R5cGUiOiAwLA0KICAgICJCYWxhbmNlR2FpbiI6IDENCiAgfSwNCiAgIlRhc2tEZXNjcmlwdGlvbiI6IHsNCiAgICAiUGFzc1NhbHRTaXplIjogMzIsDQogICAgIk4iOiAxNjM4NCwNCiAgICAiUiI6IDgsDQogICAgIlAiOiAxLA0KICAgICJEa0xlbiI6IDIsDQogICAgIlNhbHQiOiAiVlNmbEg0T2VmTlZkRllHWTl0NXdqTGZwSnlNbTdKK0szTFo1SkVDYldidz0iLA0KICAgICJTb2x1dGlvbk1hc2siOiAiLzhBPSIsDQogICAgIlNvbHV0aW9uRXF1YWwiOiAibklNPSINCiAgfQ0KfQ==";
+            var solutionB64 = "ew0KICAiQ29tbW9uSW5mbyI6IHsNCiAgICAiVGFza0d1aWQiOiAiNjM3MDQ4MjMtY2E3NS00MGU5LTkyMTgtMDAzYmJiZTY1NzIyIiwNCiAgICAiVmFsaWRVbnRpbCI6ICIyMDE1LTA3LTEwVDExOjQ3OjE2LjM0MTk0NTRaIiwNCiAgICAiVGFza1R5cGUiOiAwLA0KICAgICJCYWxhbmNlR2FpbiI6IDENCiAgfSwNCiAgIlRhc2tTb2x1dGlvbiI6IHsNCiAgICAiU29sdXRpb25QYXNzIjogIkRsMmJOSy9obGxOeDVyamNhOS9lcDBaNzZ2Rk53N0Izd0hGN2w5azNTMlE9Ig0KICB9DQp9";
+            var task =
+                Encoding.UTF8.GetString(Convert.FromBase64String(taskB64))
+                    .ParseJsonToType<ComputableTaskDescription<ScryptTaskDescription>>();
+            var soluion =
+                Encoding.UTF8.GetString(Convert.FromBase64String(solutionB64))
+                    .ParseJsonToType<ComputableTaskSolution<ScryptTaskSolution>>();
+            _output.WriteLine(task.WriteObjectToJson());
+            _output.WriteLine(soluion.WriteObjectToJson());
+            //Assert.True(await ScryptTaskSolver.CheckTaskSolution(task, soluion));
+            var passBytes = soluion.TaskSolution.SolutionPass;
+            var res = ScryptTaskSolver.ScryptCompute(
+                passBytes,
+                task.TaskDescription,
+                ScryptTaskSolver.EUseNativeScrypt.WinDll
+            );
+            _output.WriteLine(MiscFuncs.ToBinaryString(res));
+            _output.WriteLine(MiscFuncs.ToBinaryString(task.TaskDescription.SolutionMask));
+            _output.WriteLine(MiscFuncs.ToBinaryString(task.TaskDescription.SolutionEqual));
+        }
+
+        [Fact]
+        public void TestNativeManagedScryptEqual(
+            ScryptTaskSolver.EUseNativeScrypt native 
+                = ScryptTaskSolver.EUseNativeScrypt.WinDll
+        )
+        {
+            const int dkLen = 32;
+            var taskDesc = new ScryptTaskDescription()
+            {
+                N = 32768,
+                R = 8,
+                P = 1,
+                DkLen = dkLen,
+                PassSaltSize = 32,
+                Salt = new byte[32],
+                SolutionMask = new byte[dkLen],
+                SolutionEqual = new byte[dkLen]
+            };
+            var passBytes = new byte[taskDesc.PassSaltSize];
+            for (int i = 0; i < 30; i++)
+            {
+                MiscFuncs.GetRandomBytes(passBytes);
+                var r1 = ScryptTaskSolver.ScryptCompute(
+                    passBytes, taskDesc, ScryptTaskSolver.EUseNativeScrypt.None
+                );
+                Assert.Equal(
+                    r1,
+                    ScryptTaskSolver.ScryptCompute(
+                        passBytes, taskDesc, native
+                    )
+                );
+            }
+        }
+
+        [Fact]
+        public async Task TestComputeTime()
+        {
+            int cpuCount = Environment.ProcessorCount;
+            _output.WriteLine("Cpu count {0}", cpuCount);
+            const int dkLen = 32;
+            var taskDesc = new ScryptTaskDescription()
+            {
+                N = 32768,
+                R = 8,
+                P = 1,
+                DkLen = dkLen,
+                PassSaltSize = 32,
+                Salt = new byte[32],
+                SolutionMask = new byte[dkLen],
+                SolutionEqual = new byte[dkLen]
+            };
+            const int tryCount = 100;
+            int maxThreads = 4;
+            _output.WriteLine("Thread count {0}", maxThreads);
+            var lockSem = new SemaphoreSlim(maxThreads);
+            var sw = new Stopwatch();
+            sw.Start();
+            var tasks = new Task<byte[]>[tryCount];
+            for (int i = 0; i < tryCount; i++)
+            {
+                var passBytes = new byte[taskDesc.PassSaltSize];
+                MiscFuncs.GetRandomBytes(passBytes);
+                tasks[i] = Task.Factory.StartNew(
+                    async () =>
+                    {
+                        using (await lockSem.GetDisposable().ConfigureAwait(false))
+                        {
+                            _output.WriteLine("{0}", DateTime.Now);
+                            return ScryptTaskSolver.ScryptCompute(
+                                passBytes, taskDesc,
+                                ScryptTaskSolver.EUseNativeScrypt.None
+                            );
+                        }
+                    },
+                    TaskCreationOptions.LongRunning
+                ).Unwrap();
+            }
+            await Task.WhenAll(tasks).ConfigureAwait(false);
+            var taskRes = tasks.Select(x => x.Result);
+            sw.Stop();
+            _output.WriteLine(
+                "{0}",
+                sw.ElapsedMilliseconds/tryCount
+            );
+        }
+        
+        [Fact]
+        public async Task TestScryptSolver()
+        {
+            const int dkLen = 1;
+            var origTaskDesc = new ScryptTaskDescription()
+            {
+                N = 32768,
+                R = 8,
+                P = 1,
+                DkLen = dkLen,
+                PassSaltSize = 32,
+                Salt = new byte[32],
+                SolutionMask = new byte[dkLen],
+                SolutionEqual = new byte[dkLen]
+            };
+            const decimal oneBitIncome = 1.0m/32.0m;
+            const long wishfulIncome = 1;
+            int n = 50;
+            var its = new MutableTuple<int,long>[n];
+            int threadCount = 4;
+            for (int i = 0; i < n; i++)
+            {
+                var taskDesc = ScryptTaskDescriptionGenerator
+                    .GetTaskDescriptionParams(
+                        wishfulIncome,
+                        DateTime.UtcNow + TimeSpan.FromHours(1.0d),
+                        oneBitIncome,
+                        origTaskDesc
+                    );
+                var sw = new Stopwatch();
+                sw.Start();
+                var debugInfo = new ScryptTaskSolver.SolveScryptTaskDebugInfo();
+                var rightResult = await ScryptTaskSolver.SolveScryptTask(
+                    taskDesc,
+                    CancellationToken.None,
+                    threadCount,
+                    ScryptTaskSolver.EUseNativeScrypt.WinDll,
+                    debugInfo
+                ).ConfigureAwait(false);
+                sw.Stop();
+                its[i] = new MutableTuple<int, long>()
+                {
+                    Item1 = debugInfo.IterationCount,
+                    Item2 = sw.ElapsedMilliseconds
+                };
+                _output.WriteLine(
+                    "Elapsed {0}ms iteration {1} pass {2}",
+                    sw.ElapsedMilliseconds,
+                    debugInfo.IterationCount,
+                    Convert.ToBase64String(rightResult.TaskSolution.SolutionPass)
+                );
+            }
+            _output.WriteLine("Avg its {0}", its.Select(x => x.Item1).Average());
+            _output.WriteLine(
+                "Avg compute time {0} for {1} threads", 
+                its.Select(x => x.Item2).Sum() / its.Select(x => x.Item1).Sum(),
+                threadCount
+            );
+        }
+    }
+}
